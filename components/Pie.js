2a079b1f256394539a273f87056c6edee28e39d5
Nicolas <nicolasmoralesj03@gmail.com>
2023-02-19 00:44:25 -0300

    se agrega pag 404 y solucionan detalles

--- components/Pie.js
+++ components/Pie.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Pie = () => {
   return (
-    <footer className='bg-dark flex-row d-flex justify-content-center align-items-center w-100  position-absolute text-white '>
+    <footer className='bg-dark flex-row d-flex justify-content-center align-items-center w-100  position-relative bottom-0 text-white '>
         copyright Nicolas Morales - 2023
     </footer>
   )
